76e6d6b3bf1c9189eb1030faed8169b5de0b83dd
Saikiran5604 <165023824+Saikiran5604@users.noreply.github.com>
2025-03-20 21:20:59 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Lab done by Saikiran"; 
+  cout << "Lab done by Saikiran \n"; 
   return 0;
 }
 // Modified for testing GitHub Webhook

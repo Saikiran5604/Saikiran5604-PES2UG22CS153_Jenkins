7d10b79f2f80ed01f68e2f1cf991f70f06d6184a
Saikiran5604 <165023824+Saikiran5604@users.noreply.github.com>
2025-03-20 21:48:53 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,16 +1,7 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  cout << "Lab done by Saikiran \n"; 
-  return 0;
+int main() {
+    cout << "Hello, Jenkins Pipeline!" << endl;
+    return 0;
 }
-// Modified for testing GitHub Webhook
-// Another modification for testing
-// Saikiran completed Lab 8
-// Done by Saikiran
-// Done by SaikiranReddy
